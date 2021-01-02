3077eb61b0174b67177337a948e12f6e73c6be48
Alex Dyukov <alex.dyukov.93@gmail.com>
2021-01-02 17:44:03 +0000

    Add docker-compose run example

--- mytest1/test_psql_connect.py
+++ mytest1/test_psql_connect.py
@@ -5,5 +5,5 @@ from lib import psql
 
 def test_psql_connect(mydb1):
     """Test example with PostgreSQL connection"""
-    output = psql.execute(mydb1, 'SELECT 1 FROM dual', ())
+    output = psql.execute_sql(mydb1, 'SELECT 1', ())
     assert output[0] == (1,)
